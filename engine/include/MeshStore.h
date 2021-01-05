73c03cd5a093c6e56d76521e58f098e77b49e24b
Amy <dragonhel5@gmail.com>
2021-01-05 21:31:58 +0100

    fix things

--- engine/include/MeshStore.h
+++ engine/include/MeshStore.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <vector>
 #include <memory>
+#include <FBX/TriangulateProcess.h>
 #include "Mesh.h"
 
 namespace Vixen {
@@ -12,16 +13,16 @@ namespace Vixen {
         explicit MeshStore(const std::unique_ptr<LogicalDevice> &logicalDevice) : logicalDevice(logicalDevice) {}
 
         void loadMesh(const std::string &path) {
-            const auto &fbxScene = FBX::importFile("Fox.FBX", FBX::Process::TRIANGULATE);
+            const auto &fbxScene = FBX::importFile("Fox.FBX", {new FBX::TriangulateProcess()});
 
-            for (const auto &fbxMesh : fbxScene.meshes) {
+            for (const auto &fbxMesh : fbxScene->models) {
                 std::vector<glm::vec3> vertices;
                 std::vector<uint32_t> indices;
 
-                for (const auto &v : fbxMesh.vertices)
+                for (const auto &v : fbxMesh->mesh->vertices)
                     vertices.emplace_back(v.x, v.y, v.z);
 
-                for (const auto &face : fbxMesh.faces) {
+                for (const auto &face : fbxMesh->mesh->faces) {
                     indices.push_back(face[0]);
                     indices.push_back(face[1]);
                     indices.push_back(face[2]);
